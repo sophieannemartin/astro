807c9716a17764b6dec1761415d0e40dbf8d0219
Sophie <sophiedarrouxmartin@gmail.com>
2018-11-05 11:31:51 +0000

    edges function added 05/11
    Added function to remove edges from the data

--- image_processor.py
+++ image_processor.py
@@ -11,14 +11,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path = '/Users/sophie/Desktop/Work/Year 3/Lab/Astro/A1_mosaic.fits'
-pixels = fits.getdata(path).flatten()
+pixels = fits.getdata(path)
+pix_values = pixels.flatten()
 
+#Remove extreme values out
+pixelsf = pix_values[pix_values <= 3600]
+pixelsf = pixelsf[pixelsf >= 3300]
 
-pixels_masked = pixels[pixels <= 3600]
-pixels_masked = pixels_masked[pixels_masked >= 3300]
 plt.figure(0)
-n = plt.hist(pixels_masked, 300, color='purple')
+n = plt.hist(pixelsf, 300, color='#a42b44', range=(3300,3600), label='unmasked')
 plt.xlabel('Counts')
 plt.ylabel('Number of pixels')
 plt.title('Histogram bins=300')
+
+
+def remove_edges(width, data):
+    
+    """
+    Removes edges assuming same width all around
+    """
+    
+    mask = np.zeros(data.shape)
+    mask[:width, :] = 1
+    mask[-width:, :] = 1
+    mask[:, :width] = 1
+    mask[:, -width:] = 1
+
+    data_noedges = np.ma.masked_array(data, mask)
+    return data_noedges
+
+pixels_noedges = remove_edges(115, pixels).flatten()
+
+n = plt.hist(pixels_noedges.compressed(), 300, color='#ff9900', 
+             range=(3300,3600), label='masked')
+plt.xlabel('Counts')
+plt.ylabel('Number of pixels')
+plt.title('Histogram bins=300')
+plt.legend()
 
