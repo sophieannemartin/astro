a5d230888e6a682a65f059ad88fc731cfbd6533b
Sophie <sophiedarrouxmartin@gmail.com>
2018-11-16 10:16:56 +0000

    Update

--- run_image_mask.py
+++ run_image_mask.py
@@ -8,7 +8,7 @@ Created on Mon Nov 12 15:10:58 2018
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
 from astropy.io import fits
-import image_functions as image
+import functions.image_functions as image
 import numpy as np
 from scipy.optimize import curve_fit
 
@@ -40,8 +40,6 @@ no_star4 = image.remove_star((2284,906), 29,no_star3)
 no_star5 = image.remove_star((3759,2133),28,no_star4)
 no_star6 = image.remove_star((2312,2131), 26, no_star5)
 
-no_star6f = no_star6.flatten()
-
 #removing horizontal bleeding from main bleed from central star 
 no_horiz1 = image.remove_exp_bleeding1(1447,1651,426,70,-0.018,no_star6)
 no_horiz2 = image.remove_exp_bleeding2(1102,1429,426,70,0.01,no_horiz1)
@@ -61,14 +59,14 @@ no_block2 = image.remove_strip(1642,1647,334,354,no_block1)
 no_block3 = image.remove_strip(1027,1042,424,451,no_block2)
 
 plt.subplot(1,2,2)
-plt.imshow(no_star4, norm = LogNorm(), origin = 'lower')
+plt.imshow(no_block3, norm = LogNorm(), origin = 'lower')
 plt.title('edited image')
 
-mask = no_star4.flatten() < 3600
+mask = no_block3.flatten() < 3600
 mask = mask > 3300
-no_star4m = np.ma.masked_array(no_star4, mask).compressed()
+no_block3m = np.ma.masked_array(no_block3, mask).compressed()
 
-hist, bin_edges = np.histogram(no_star4m,bins=300, range=(3300,3600))
+hist, bin_edges = np.histogram(no_block3m,bins=300, range=(3300,3600))
 bin_centres = (bin_edges[:-1] + bin_edges[1:])/2
 
 # Define model function to be used to fit to the data above:
@@ -86,12 +84,10 @@ sigma = coeff[2]
 # Get the fitted curve
 hist_fit = gauss(bin_centres, *coeff)
 plt.figure()
-plt.hist(no_star4m, bins=300,range=(3300,3600), label='Histogram')
+plt.hist(no_block3m, bins=300,range=(3300,3600), label='Histogram')
 plt.plot(bin_centres, hist_fit, label='Fitted data')
 plt.legend()
 plt.xlabel('Counts')
 plt.ylabel('Number of pixels')
-plt.show()
-
 
 
