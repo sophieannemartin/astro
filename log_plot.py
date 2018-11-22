d1536a51d17670801683bce5ce460025cd39c463
Sophie <sophiedarrouxmartin@gmail.com>
2018-11-22 15:32:21 +0000

    logplot func

--- log_plot.py
+++ log_plot.py
@@ -6,3 +6,26 @@ Created on Wed Nov 21 21:39:02 2018
 @author: sophie
 """
 
+import numpy as np
+import pandas as pd
+
+def create_histogram_heights(catalog, range_, bins):
+    
+    # a dataframe
+    hist_heights, edges = np.histogram(catalog['magnitude'].values, 
+                               range=(range_[0], range_[1]), bins=bins)
+    
+    df = pd.DataFrame({'Bin_heights':hist_heights, 'Edges':edges})
+    return df
+
+def save_heights(df):
+    
+    writer = pd.ExcelWriter('histogram_of_magnitudes.xlsx')
+    df.to_excel(writer)
+    writer.save()
+    
+    
+def save_log_data(catalog, range_, bins):
+    df = create_histogram_heights
+    save_heights(df)
+    return ddf
